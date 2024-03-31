2f05ee1fa9280aefd39acad5f48d86a19cc2265a
vladislav-vov <128249554+vladislav-vov@users.noreply.github.com>
2024-04-01 00:15:08 +0300

    Верстка шапки и первой секции

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,37 @@
 import './libs/dynamicAdapt.js';
 
 import '../scss/style.scss';
+
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+
+import 'swiper/css';
+
+const currentSlideEkem = document.querySelector('.promo__slider-current');
+const totalSlidesElem = document.querySelector('.promo__slider-total');
+
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation],
+  loop: true,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.promo__slider-next',
+    prevEl: '.promo__slider-prev',
+  },
+  on: {
+    init: async function (swiper) {
+      totalSlidesElem.textContent =
+        swiper.slides.length < 9
+          ? `0${swiper.slides.length}`
+          : swiper.slides.length;
+    },
+    slideChange: function (swiper) {
+      const index =
+        swiper.realIndex < 9
+          ? `0${swiper.realIndex + 1}`
+          : swiper.realIndex + 1;
+
+      currentSlideEkem.textContent = index;
+    },
+  },
+});
