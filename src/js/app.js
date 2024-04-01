d2c5baaf0c81c68a71c8ec66bdc93dd5f50f9643
vladislav-vov <128249554+vladislav-vov@users.noreply.github.com>
2024-04-01 19:05:28 +0300

    Верстка четырех секций и реализация меню

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,16 @@
 import './libs/dynamicAdapt.js';
 
-import '../scss/style.scss';
-
 import Swiper from 'swiper';
 import { Navigation } from 'swiper/modules';
 
 import 'swiper/css';
 
+import '../scss/style.scss';
+
 const currentSlideEkem = document.querySelector('.promo__slider-current');
 const totalSlidesElem = document.querySelector('.promo__slider-total');
 
-const swiper = new Swiper('.swiper', {
+new Swiper('.swiper', {
   modules: [Navigation],
   loop: true,
   spaceBetween: 30,
@@ -35,3 +35,37 @@ const swiper = new Swiper('.swiper', {
     },
   },
 });
+
+const parent = document.querySelector('.projects__list');
+const childs = parent.querySelectorAll('.projects__item');
+
+function resizeHeadings() {
+  childs.forEach((child) => {
+    const heading = child.querySelector('.projects__item-title');
+    const parentWidth = child.offsetWidth;
+    const headingSize = Math.min(parentWidth * 0.12, 64);
+    heading.style.fontSize = headingSize + 'px';
+  });
+}
+
+resizeHeadings();
+
+window.addEventListener('load', resizeHeadings);
+window.addEventListener('resize', resizeHeadings);
+
+const bodyLockToggle = () => {
+  if (!document.documentElement.classList.contains('lock')) {
+    document.documentElement.classList.add('lock');
+  } else {
+    document.documentElement.classList.remove('lock');
+  }
+};
+
+if (document.querySelector('.menu__btn')) {
+  document.addEventListener('click', function (e) {
+    if (e.target.closest('.menu__btn')) {
+      bodyLockToggle();
+      document.documentElement.classList.toggle('menu-open');
+    }
+  });
+}
